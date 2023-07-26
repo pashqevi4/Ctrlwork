e3e4e49f9ac488a7302b3c2555335e998150f71f
pashqa <pashqevi4@gmail.com>
2023-07-26 20:43:55 +0700

    lists added

--- task.java
+++ task.java
@@ -10,7 +10,8 @@ public class task {
         task1();
     }
     static void task1(){
-        
+        List<String> list = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
 
     }
 
