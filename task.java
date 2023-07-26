b70ded51982ea0a4706754459011c7ec07dcf7b4
pashqa <pashqevi4@gmail.com>
2023-07-26 20:40:34 +0700

    task start

---
+++ task.java
@@ -0,0 +1,18 @@
+package Ctrlwork;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class task {
+    public static void main(String[] args) {
+        task1();
+    }
+    static void task1(){
+        
+
+    }
+
+
+}
